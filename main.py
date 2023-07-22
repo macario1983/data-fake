c47c554393c1116e115076d16803173d399b837d
Diego Macario <diego_macario@hotmail.com>
2023-07-22 13:32:03 -0300

    Alterando a idade

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ fake = Faker("pt_BR")
 
 for _ in range(10_000):
     nome = fake.name()
-    idade = random.randint(5, 19)
+    idade = random.randint(3, 17)
     email = fake.email()
     sexo = random.choice(["M", "F"])
     print('INSERT INTO aluno (nome, idade, email, sexo) VALUES ' f"('{nome}', {idade}, '{email}', '{sexo}');")
